e8b8d4a6ee2ac053a807fd12784c559da0fe2491
Björn Ganslandt <b.ganslandt@gmail.com>
2021-10-08 10:42:00 +0200

    Use null-ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,49 +1,49 @@
 -- Load shared config
-require("shared-config")
+require 'shared-config'
 
 -- Install packer
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 vim.api.nvim_exec(
-	[[
+  [[
   augroup Packer
     autocmd!
     autocmd BufWritePost init.lua PackerCompile
   augroup end
 ]],
-	false
+  false
 )
 
-local use = require("packer").use
-require("packer").startup(function()
-	use("wbthomason/packer.nvim")
-	use("shaunsingh/nord.nvim")
-	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	use({ "prettier/vim-prettier", run = "yarn install" })
-	use("chrisbra/vim-commentary")
-	use("editorconfig/editorconfig-vim")
-	use("hoob3rt/lualine.nvim")
-	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
-	use("windwp/nvim-autopairs")
-	use("mattn/emmet-vim")
-	use("svermeulen/vim-subversive")
-	use("neovim/nvim-lspconfig")
-	use("kabouzeid/nvim-lspinstall")
-	use("kyazdani42/nvim-tree.lua")
-	use({ "vuki656/package-info.nvim", requires = "MunifTanjim/nui.nvim" })
-	use("hrsh7th/vim-vsnip")
-	use("hrsh7th/vim-vsnip-integ")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-vsnip")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("norcalli/nvim-colorizer.lua")
-	use("rhysd/clever-f.vim")
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'shaunsingh/nord.nvim'
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'chrisbra/vim-commentary'
+  use 'editorconfig/editorconfig-vim'
+  use 'hoob3rt/lualine.nvim'
+  use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
+  use 'windwp/nvim-autopairs'
+  use 'mattn/emmet-vim'
+  use 'svermeulen/vim-subversive'
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall'
+  use 'kyazdani42/nvim-tree.lua'
+  use { 'vuki656/package-info.nvim', requires = 'MunifTanjim/nui.nvim' }
+  use 'hrsh7th/vim-vsnip'
+  use 'hrsh7th/vim-vsnip-integ'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'norcalli/nvim-colorizer.lua'
+  use 'rhysd/clever-f.vim'
+  use 'jose-elias-alvarez/null-ls.nvim'
 end)
 
 -- Save when switching buffers
@@ -51,157 +51,173 @@ vim.o.autowriteall = true
 -- Make line numbers default
 vim.wo.number = true
 -- Add line width marker
-vim.o.colorcolumn = "81"
+vim.o.colorcolumn = '81'
 -- Show signs in the number column
-vim.wo.signcolumn = "number"
+vim.wo.signcolumn = 'number'
 
 -- Highlight on yank
 vim.api.nvim_exec(
-	[[
+  [[
   augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
 ]],
-	false
+  false
 )
 
 -- Start terminal in insert mode
 vim.api.nvim_exec(
-	[[
+  [[
   augroup terminal
     au TermOpen * startinsert
   augroup END
 ]],
-	false
+  false
 )
 
 -- Use ESC to exit insert mode in :term
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
 
 -- Theme
 vim.o.termguicolors = true
 vim.g.nord_borders = true
-vim.cmd([[colorscheme nord]])
+vim.cmd [[colorscheme nord]]
 
 -- PLUGINS
 
 -- Telescope
-vim.api.nvim_set_keymap("n", "<C-p>", "<Cmd> Telescope find_files<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-f>", "<Cmd> Telescope live_grep<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-p>', '<Cmd> Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-f>', '<Cmd> Telescope live_grep<CR>', { noremap = true })
 
 -- Tree
 vim.g.nvim_tree_quit_on_open = 1
-require("nvim-tree").setup({
-	update_focused_file = {
-		enable = true,
-	},
-})
-vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true })
-
--- Prettier
--- vim.g['prettier#autoformat_config_present'] = 1
-vim.api.nvim_exec(
-	[[
-    autocmd BufWritePre *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue PrettierAsync
-  ]],
-	false
-)
+require('nvim-tree').setup {
+  update_focused_file = {
+    enable = true,
+  },
+}
+vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true })
 
 -- LSP
-lspconfig = require("lspconfig")
+lspconfig = require 'lspconfig'
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-require("lspinstall").setup()
-local servers = require("lspinstall").installed_servers()
+require('lspinstall').setup()
+local servers = require('lspinstall').installed_servers()
 for _, server in pairs(servers) do
-	lspconfig[server].setup({
-		capabilities = capabilities,
-	})
+  lspconfig[server].setup {
+    capabilities = capabilities,
+    on_attach = function(client)
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
+    end,
+  }
 end
 
-vim.api.nvim_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "ga", "<Cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "ga", "<Cmd>lua vim.lsp.buf.range_code_action()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'ga', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'ga', '<Cmd>lua vim.lsp.buf.range_code_action()<CR>', { noremap = true, silent = true })
+
+-- Null-LS
+require('null-ls').config {
+  sources = {
+    -- Formatters
+    require('null-ls').builtins.formatting.prettierd,
+    require('null-ls').builtins.formatting.stylua,
+
+    -- Linters
+    require('null-ls').builtins.diagnostics.eslint_d,
+  },
+}
+
+lspconfig['null-ls'].setup {
+  -- Format on save
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd 'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()'
+    end
+  end,
+}
 
 -- CMP
-local cmp = require("cmp")
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
-		end,
-	},
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn['vsnip#anonymous'](args.body)
+    end,
+  },
 
-	mapping = {
-		["<S-Tab>"] = cmp.mapping.select_prev_item(),
-		["<Tab>"] = cmp.mapping.select_next_item(),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-	},
+  mapping = {
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+  },
 
-	sources = {
-		{ name = "path" },
-		{ name = "buffer" },
-		{ name = "vsnip" },
-		{ name = "nvim_lsp" },
-	},
-})
+  sources = {
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'vsnip' },
+    { name = 'nvim_lsp' },
+  },
+}
 
-vim.o.completeopt = "menuone,noinsert,noselect"
-vim.o.shortmess = vim.o.shortmess .. "c"
+vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.o.shortmess = vim.o.shortmess .. 'c'
 
 -- Package info
-require("package-info").setup()
+require('package-info').setup()
 
 -- Autopair
-require("nvim-autopairs").setup()
+require('nvim-autopairs').setup()
 
-require("nvim-autopairs.completion.cmp").setup({
-	map_cr = true, --  map <CR> on insert mode
-	map_complete = true, -- it will auto insert `(` after select function or method item
-	auto_select = true, -- automatically select the first item
-})
+require('nvim-autopairs.completion.cmp').setup {
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true, -- it will auto insert `(` after select function or method item
+  auto_select = true, -- automatically select the first item
+}
 
 -- Git Signs
-require("gitsigns").setup()
+require('gitsigns').setup()
 
 -- Lualine
-require("lualine").setup({
-	options = { theme = "nord" },
-	sections = {
-		lualine_a = { { "mode", upper = true } },
-		lualine_b = { { "filename", file_status = true } },
-		lualine_c = { { "diagnostics", sources = { "nvim_lsp" } } },
-		lualine_x = { "encoding", "filetype" },
-		lualine_y = { { "branch", icon = "" } },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	extensions = { "quickfix", "nvim-tree" },
-})
+require('lualine').setup {
+  options = { theme = 'nord' },
+  sections = {
+    lualine_a = { { 'mode', upper = true } },
+    lualine_b = { { 'filename', file_status = true } },
+    lualine_c = { { 'diagnostics', sources = { 'nvim_lsp' } } },
+    lualine_x = { 'encoding', 'filetype' },
+    lualine_y = { { 'branch', icon = '' } },
+    lualine_z = { 'location' },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  extensions = { 'quickfix', 'nvim-tree' },
+}
 
 -- Clever f
 vim.g.clever_f_across_no_line = 1
 vim.g.clever_f_smart_case = 1
-vim.g.clever_f_mark_char_color = "IncSearch"
+vim.g.clever_f_mark_char_color = 'IncSearch'
 
 -- Colorizer
-require("colorizer").setup({
-	css = { rgb_fn = true },
-	scss = { rgb_fn = true },
-	sass = { rgb_fn = true },
-	stylus = { rgb_fn = true },
-	vim = { names = false },
-	tmux = { names = false },
-})
+require('colorizer').setup {
+  css = { rgb_fn = true },
+  scss = { rgb_fn = true },
+  sass = { rgb_fn = true },
+  stylus = { rgb_fn = true },
+  vim = { names = false },
+  tmux = { names = false },
+}
--- .config/nvim/lua/shared-config.lua
+++ .config/nvim/lua/shared-config.lua
@@ -6,20 +6,20 @@ vim.o.smartcase = true
 vim.o.autoindent = true
 
 -- Use system clipboard
-vim.o.clipboard = "unnamed"
+vim.o.clipboard = 'unnamed'
 
 -- indent by 2 spaces by default
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 -- Emmet
-vim.g.user_emmet_leader_key = ","
+vim.g.user_emmet_leader_key = ','
 
 -- Y yank until the end of line  (note: this is now a default on master)
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
 
 -- s for substitute
-vim.api.nvim_set_keymap("n", "s", "<plug>(SubversiveSubstitute)", {})
-vim.api.nvim_set_keymap("n", "ss", "<plug>(SubversiveSubstituteLine)", {})
-vim.api.nvim_set_keymap("n", "S", "<plug>(SubversiveSubstituteToEndOfLine)", {})
-vim.api.nvim_set_keymap("x", "s", "p", {})
+vim.api.nvim_set_keymap('n', 's', '<plug>(SubversiveSubstitute)', {})
+vim.api.nvim_set_keymap('n', 'ss', '<plug>(SubversiveSubstituteLine)', {})
+vim.api.nvim_set_keymap('n', 'S', '<plug>(SubversiveSubstituteToEndOfLine)', {})
+vim.api.nvim_set_keymap('x', 's', 'p', {})
