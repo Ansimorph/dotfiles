20f1115723bb91aaa4dd0f21b35b45be6334204c
Björn Ganslandt <b.ganslandt@gmail.com>
2024-08-04 17:49:43 +0200

    Remove software

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ source ~/bin/setup-b2
 fzf --fish | source
 
 # Aliases
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias e="$EDITOR"
-abbr G.. 'cd (git rev-parse --show-toplevel || echo .)'
+abbr serve "python3 -m http.server 8080"
+abbr G.. "cd (git rev-parse --show-toplevel || echo .)"
