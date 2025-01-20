8093995dff1d9f0d78bf440e5b33c5b7ebf1aecc
Björn Ganslandt <b.ganslandt@gmail.com>
2025-01-20 15:29:16 +0100

    Add helper scripts

---
+++ bin/react-component-template/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import styles from "./TEMPLATE.module.scss";
+
+const TEMPLATE: FunctionComponent = () => {
+  return <div></div>;
+};
+
+export default TEMPLATE;
