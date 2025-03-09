bad2287f0e602a626be597eb7f5dacb737b51ff7
Björn Ganslandt <b.ganslandt@gmail.com>
2025-03-09 11:37:21 +0100

    Install aerc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,6 @@ fzf --fish | source
 # Aliases
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias e="$EDITOR"
+alias m="aerc"
 abbr serve "python3 -m http.server 8080"
 abbr G.. "cd (git rev-parse --show-toplevel || echo .)"
