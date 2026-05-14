dec7ffc52ff4f3a4db7588429efa9bc0f5811f99
Björn Ganslandt <hi@ganslandt.xyz>
2026-05-14 09:57:44 +0200

    Add keyboard brightness control

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ eval (ssh-agent -c | head -n2)
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias e="$EDITOR"
 alias m="aerc"
+abbr screenrecorder 'wf-recorder -g "$(slurp)"'
 abbr gco "git checkout"
 abbr serve "python3 -m http.server 8080"
 abbr G.. "cd (git rev-parse --show-toplevel || echo .)"
